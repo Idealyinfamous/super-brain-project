da83c16b55d7d58aee2501e23f682d2d01722109
Chris_Gruen <113554860+ChristyGruen@users.noreply.github.com>
2023-10-15 16:36:27 -0500

    update server.py with apicheck route

--- flask_server/server.py
+++ flask_server/server.py
@@ -1,4 +1,10 @@
 from flask import Flask
+import urllib.request, json
+import os
+from dotenv import load_dotenv
+load_dotenv()
+apiKey=os.getenv('APIKEY')
+import requests
 
 app = Flask(__name__)
 
@@ -6,6 +12,22 @@ app = Flask(__name__)
 def test():
     return {"test": ["thing1", "thing2", "thing3"]}
 
+@app.route("/apicheck")
+def apicheck():
+    url = f'https://api.spoonacular.com/recipes/findByIngredients?apiKey={apiKey}&ingredients=chicken,+pasta,+tomatoes&number2'
+    
+    r = requests.get(url)
+    # print(r)
+    return(r.json())
+    # return{"list": ["li", "list", "listy"]}
+
+    # response = urllib.request.urlopen(url)
+    # data = response.read()
+    # dict = json.loads(data)
+
+    # return (dict)
+    # print(url)
+    # return {"test": ["chicken pants", "fish umbrella", "bird box"]}
 
 if __name__ == "__main__":
     app.run(debug=True)
