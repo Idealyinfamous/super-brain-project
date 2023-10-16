1fabcdd2872d595d561f2b3db973eec14028cb7c
kalikokalikova <calico.seders@gmail.com>
2023-10-16 11:00:32 -0700

    basic user registration and login via Passage

---
+++ client/src/pages/PassagePage.js
@@ -0,0 +1,10 @@
+import "@passageidentity/passage-elements/passage-auth";
+
+function Passage() {
+    return (
+        // <passage-auth app-id={process.env.REACT_APP_PASSAGE_APP_ID}></passage-auth>
+        <passage-auth app-id={"1rXH3X8YmXW5rDsymCChlH9J"}></passage-auth>
+    );
+}
+
+export default Passage;
