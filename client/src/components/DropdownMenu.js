875162c8a5203f249568f5ac8f7beadfc3bac517
kalikokalikova <calico.seders@gmail.com>
2023-10-15 21:45:26 -0700

    Dropdown menu gets options from massive csv file in flask

--- client/src/components/DropdownMenu.js
+++ client/src/components/DropdownMenu.js
@@ -1,8 +1,23 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react'
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
+import axios from "axios";
+
 
 export default function Tags() {
+  const [ingredients, setIngredients] = useState([{}])
+  useEffect(() => {
+    axios.get("/ingredients")
+      .then((response) => {
+        console.log(response.data)
+        setIngredients(response.data)
+      })
+      .catch((error) => {
+        console.log("Here's your error: , error")
+      });
+  }, [])
+
+  // TODO fix the non-unique key error
   return (
       <Autocomplete
         multiple
@@ -20,17 +35,3 @@ export default function Tags() {
   );
 }
 
-// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
-const ingredients = [
-  { name: 'butter' },
-  { name: 'milk' },
-  { name: 'lentils' },
-  { name: 'eggs' },
-  { name: 'summer squash' },
-  { name: 'heirloom tomato' },
-  { name: 'hot dogs' },
-  { name: 'corn tortillas' },
-  { name: 'salsa' },
-  { name: 'garlic' },
-  { name: 'onion' },
-];
--- flask_server/controllers/recipes.py
+++ flask_server/controllers/recipes.py
@@ -1,8 +1,22 @@
 from flask_server import app
 from flask import render_template, redirect, request, session
 
-
 # Homepage
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/ingredients')
+def ingredients():
+    return parse_ingredients_list()
+
+def parse_ingredients_list():
+    results = []
+    with open("./ingredients.csv", "r") as file:
+        for line in file:
+            parts = line.split(";")
+            ingredient = {}
+            ingredient["name"] = parts[0]
+            ingredient["id"] = parts[1]
+            results.append(ingredient)
+    return results
