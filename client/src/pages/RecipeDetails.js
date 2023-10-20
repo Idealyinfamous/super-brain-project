ee45fddb80c3d1861a03eeaad0268781b28ac38e
Jessica Perry <l33thedgieperry@gmail.com>
2023-10-20 16:22:24 -0700

    removed login prompt in RecipeDetails

--- client/src/pages/RecipeDetails.js
+++ client/src/pages/RecipeDetails.js
@@ -1,5 +1,4 @@
 import React from "react";
-import LogInPrompt from "../components/LogInPrompt";
 import { PassageAuthGuard } from "@passageidentity/passage-react";
 import { usePassageUserInfo } from "../hooks/";
 import { PlusCircle, CaretLeftFill } from 'react-bootstrap-icons'
@@ -42,7 +41,7 @@ function RecipeDetails() {
 
   return (
     <>
-      <LogInPrompt />
+
       <div id="recipe-details-section">
 
         <h1 className='text-center'>Recipe Name</h1>
