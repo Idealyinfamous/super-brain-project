f0ef7e03fbb2d5237d5b5c46ac84397ef7bba374
elaine2700 <easerrano.e@gmail.com>
2023-10-20 16:52:59 -0700

    Styling suggesting recipes -first part

--- client/src/pages/SuggestedRecipe.js
+++ client/src/pages/SuggestedRecipe.js
@@ -17,9 +17,45 @@ import JSConfetti from "js-confetti";
 
 
 function SuggestedRecipe() {
+
+  const dummyIngredients = ["Onions", "Potatoes", "Chicken", "Salt", "Oil"]
+  const description = "The best recipe of all times."
+  const recipeName = "Chocolate Cake"
+  const prepTime = '30 min';
+  const totalTime = '50min'
+
+
   return (
     <>
-      <h1>SuggestedRecipe</h1>
+      <div className="wrapper">
+        <div>
+          <h1>Here is your <span className="brown">recipe</span></h1>
+          <div className="card card-grid">
+            <div className="card-image-container">
+              <img />
+            </div>
+            <div className="card-details-container">
+              <h2>{recipeName}</h2>
+
+              <ul className="ingredients">
+                {
+                  dummyIngredients.map((ingredient)=>(
+                    <li className="ingredient">{ingredient}</li>
+                  ))
+                }
+              </ul>
+              <div>
+                <p>Prep Time</p>
+                <p>Total Time</p>
+              </div>
+              <h3>Description</h3>
+              <p>Lorem Ipsaoifvgasghasgjasfjoais</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
     </>
   );
 }
