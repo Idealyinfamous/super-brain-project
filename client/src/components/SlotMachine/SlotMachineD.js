9490ef1a01f543dda849a59ab9488edac526dc2c
elaine2700 <easerrano.e@gmail.com>
2023-10-21 11:04:58 -0700

    Integrating Slot Machine to Suggested Recipe

---
+++ client/src/components/SlotMachine/SlotMachineD.js
@@ -0,0 +1,51 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import "./SlotMachine.scss";
+import foodstrip from "./foodstrip.png";
+import "./slotMachineFunctionality.js";
+import {useState, useEffect} from 'react';
+
+export default function SlotMachineD() {
+  const [open, setOpen] = useState(false);
+
+  useEffect(()=>{
+    setOpen(true);
+  })
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+        {
+            /*<Button variant="outlined" onClick={handleClickOpen}>
+        Open slot dialog
+      </Button>*/
+        }
+      
+
+        <DialogContent>
+          <div className="slots">
+            <div className="reel"></div>
+            <div className="reel"></div>
+            <div className="reel"></div>
+          </div>
+
+          <div id="debug" className="debug"></div>
+
+        </DialogContent>
+
+
+    </div>
+  );
+}
--- client/src/pages/SuggestedRecipe.js
+++ client/src/pages/SuggestedRecipe.js
@@ -1,7 +1,7 @@
 import { React, useEffect, useState } from "react";
 import { useParams, Link } from 'react-router-dom';
 import { PlusCircle, CaretLeftFill, CaretRightFill } from "react-bootstrap-icons";
-import SlotMachine from "../components/SlotMachine/SlotMachine";
+import SlotMachineD from "../components/SlotMachine/SlotMachineD";
 
 import { styled } from "@mui/material/styles";
 import {
@@ -24,6 +24,9 @@ const SPOON_API_KEY = "211a5be1593a4998b32368d3b3517ac6";
 const SPOON_API_URL = "https://api.spoonacular.com";
 
 function SuggestedRecipe() {
+
+  const [loading, setLoading] = useState(false);
+
   const { recipeId } = useParams();
   const [recipeInfo, setRecipeInfo] = useState([]);
 
@@ -31,15 +34,27 @@ function SuggestedRecipe() {
   let headers = { 'Content-Type': 'application/json' }
 
   useEffect(() => {
+
+    setLoading(true);
+    /*setTimeout(() => {
+      console.log("Time out");
+      setLoading(false)
+    }, 6800);*/
+
     axios
-    .get((url = url), (headers = headers))
-    .then((response) => {
-      console.log(response)
-      setRecipeInfo(response.data)
-    })
-    .catch((error) => {
-      console.log("There was an error and here it is: ", error);
-    });
+      .get((url = url), (headers = headers))
+      .then((response) => {
+        console.log(response)
+        setRecipeInfo(response.data)
+        setTimeout(() => {
+          console.log("Time out");
+          setLoading(false)
+        }, 6800);
+      })
+      .catch((error) => {
+        console.log("There was an error and here it is: ", error);
+        setLoading(false);
+      });
   }, [])
 
   const dummyIngredients = ["Onions", "Potatoes", "Chicken", "Salt", "Oil"];
@@ -51,78 +66,89 @@ function SuggestedRecipe() {
 
   return (
     <>
-      <SlotMachine />
       <div className="wrapper">
         <div>
           <h1 className="text-center">
             Here is your <span className="brown">recipe</span>
           </h1>
-          <div className="card card-grid">
-            <div className="card-image-container">
-              <img src={recipeInfo.image}/>
-            </div>
-            <div className="card-details-container">
-              <h2>{recipeInfo.title}</h2>
+          {
+            loading ?
+              (<SlotMachineD />) :
+              (
+                <div className="card card-grid">
+                  <div className="card-image-container">
+                    <img src={recipeInfo.image} />
+                  </div>
+                  <div className="card-details-container">
+                    <h2 className="text-center">{recipeInfo.title}</h2>
 
-              <ul className="ingredients">
-                {dummyIngredients.map((ingredient) => (
-                  <li className="ingredient">{ingredient}</li>
-                ))}
-              </ul>
-              <div className="times-container">
-                <div className="text-center">
-                  <p><b>Prep Time</b></p>
-                  <p>{prepTime}</p>
+                    <ul className="ingredients">
+                      {dummyIngredients.map((ingredient) => (
+                        <li className="ingredient">{ingredient}</li>
+                      ))}
+                    </ul>
+                    <div className="times-container">
+                      <div className="text-center">
+                        <p><b>Prep Time</b></p>
+                        <p>{prepTime}</p>
+                      </div>
+                      <div className="text-center">
+                        <p><b>Total Time</b></p>
+                        <p>{totalTime}</p>
+                      </div>
+                    </div>
+                    <h3>Description</h3>
+                    <p>Lorem Ipsaoifvgasghasgjasfjoais</p>
+                  </div>
                 </div>
-                <div>
-                  <p><b>Total Time</b></p>
-                  <p>{totalTime}</p>
-                </div>
+              )
+          }
+
+          {
+            loading ? (
+              <div></div>
+            ) : (
+              <div className="suggested-btns">
+                <Link to="/recipe-details">
+                  <Button
+                    style={{
+                      borderRadius: 20,
+                      backgroundColor: "#f4c17f",
+                      fontFamily: "Open-Dyslexic",
+                      color: "#6c3428",
+                      marginTop: 10,
+                    }}
+                    variant="contained"
+                    onClick={() => {
+                      console.log("Get another recipe");
+                    }}
+                  >
+                    YES I LIKE THIS<CaretRightFill></CaretRightFill>
+                  </Button>
+                </Link>
+                <Link to="/">
+                  <Button
+                    style={{
+                      borderRadius: 20,
+                      borderColor: "#6c3428",
+                      fontFamily: "Open-Dyslexic",
+                      color: "#6c3428",
+                      marginTop: 10,
+                      maxWidth: 250,
+                    }}
+                    variant="outlined"
+                    onClick={() => {
+                      console.log("Get another recipe");
+                    }}
+                  >
+                    <CaretLeftFill></CaretLeftFill>Noooo, I want to go back and try again.
+                  </Button>
+                </Link>
               </div>
-              <h3>Description</h3>
-              <p>Lorem Ipsaoifvgasghasgjasfjoais</p>
-            </div>
-          </div>
-          <div className="suggested-btns">
-            <Link to="/recipe-details">
-              <Button
-                style={{
-                  borderRadius: 20,
-                  backgroundColor: "#f4c17f",
-                  fontFamily: "Open-Dyslexic",
-                  color: "#6c3428",
-                  marginTop: 10,
-                }}
-                variant="contained"
-                onClick={() => {
-                  console.log("Get another recipe");
-                }}
-              >
-                YES I LIKE THIS<CaretRightFill></CaretRightFill>
-              </Button>
-            </Link>
-            <Link to="/">
-              <Button
-                style={{
-                  borderRadius: 20,
-                  borderColor: "#6c3428",
-                  fontFamily: "Open-Dyslexic",
-                  color: "#6c3428",
-                  marginTop: 10,
-                  maxWidth: 250,
-                }}
-                variant="outlined"
-                onClick={() => {
-                  console.log("Get another recipe");
-                }}
-              >
-                <CaretLeftFill></CaretLeftFill>Noooo, I want to go back and try again.
-              </Button>
-            </Link>
-          </div>
+            )
+          }
 
         </div>
-
       </div>
     </>
   );
